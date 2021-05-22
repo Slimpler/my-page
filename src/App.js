914bb80eed4051c4d0dbbd0524ffd9722f472eb4
nicolasd <nicolas.delgado@alumnos.uv.cl>
2021-05-22 01:43:18 -0400

    Mi Pagina

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/SigninPage";
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/signin" component={SigninPage} exact />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
